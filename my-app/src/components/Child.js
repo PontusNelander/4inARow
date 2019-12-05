6271958477f25320702467f13b84813133fcb6e0
Pontus Nelander <pontus.nelander@live.se>
2019-12-05 11:07:25 +0100

    Added pictures and states to childcomponent

--- my-app/src/components/Child.js
+++ my-app/src/components/Child.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
+import Empty from './EmptySpace.PNG';
+import redPlupp from './RedSpace.PNG';
+import gulPlupp from './YellowSpace.PNG';
 
 class Child extends Component {
+    state={isAvailable : false, red: false}
     render() {
         return (
-            <div>Child!</div>
+            <div style={{backgroundImage: this.state.isAvailable ? `url(${Empty})` : this.state.red ? `url(${redPlupp})` : `url(${gulPlupp})` }}>Child!</div>
         )
     }
 }
